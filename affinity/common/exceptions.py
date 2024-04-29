a5a81e6adf9a5b8feb38adbce5cbcb6390243998
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-04-29 11:54:49 +0100

    Update setup version and enhance exceptions
    The setup version number has been updated from 1.1.13 to 1.1.14. In addition, a new attribute 'response' has been added to the exception initializers, in both 'RequestFailed' and 'RateLimitExceeded' classes in the 'exceptions.py' file. This new attribute will contain the response information when an exception is encountered.

--- affinity/common/exceptions.py
+++ affinity/common/exceptions.py
@@ -17,16 +17,18 @@ class RequestTypeNotAllowed(AffinityBaseException):
 
 class RequestFailed(AffinityBaseException):
     """ An exception raised when a request fails """
-    def __init__(self, message: str, status_code: Optional[int] = None):
+    def __init__(self, message: str, status_code: Optional[int] = None, response: dict | None = None):
         self.message = message
+        self.response = response
         self.status_code = status_code
     pass
 
 
 class RateLimitExceeded(AffinityBaseException):
     """ An exception raised when a rate limit is exceeded """
-    def __init__(self, message: str, retry_after: int | None = None):
+    def __init__(self, message: str, retry_after: int | None = None, response: dict | None = None):
         self.message = message
+        self.response = response
         self.retry_after = retry_after
 
 
