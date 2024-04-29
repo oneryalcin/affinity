f8ff0a60e449ed49d14850ab8869fb23d1012870
Mehmet Öner Yalçın <oneryalcin@gmail.com>
2024-04-29 11:48:13 +0100

    Update project dependencies
    In setup.py, new dependencies have been added and some existing dependencies have been updated. The 'backoff', 'requests' version number, and 'loguru' packages are now required. In requirements.txt, the 'tenacity' package has been replaced with 'backoff'.

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
         author_email='natefduncan@gmail.com, oneryalcin@gmail.com',
         url="https://github.com/oneryalcin/affinity",
         packages=['affinity', 'affinity.client', 'affinity.common', 'affinity.core'],
-        install_requires=['requests>=2.28.0', "dataclasses_json>=0.6.1", "tenacity>=8.2.3"],
+        install_requires=[
+                'requests>=2.28.0',
+                "dataclasses_json>=0.6.1",
+                "backoff>=2.2.1",
+                "requests==2.31.0",
+                "loguru>=0.7.2"
+                ],
         )
 
